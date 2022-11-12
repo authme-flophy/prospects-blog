6f6f34ae9bb3d2d2c207f1498bfb06000a0cc655
waikau maina <ericmaina.dev@gmail.com>
2022-11-12 09:03:48 +0300

    Code changes from lesson#7-Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+  const handleClick = (name) => {
+    console.log(`Hello, ${name}`);
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={()=>handleClick('Waikau')}>Click Me!</button>
     </div>
    );
 }
